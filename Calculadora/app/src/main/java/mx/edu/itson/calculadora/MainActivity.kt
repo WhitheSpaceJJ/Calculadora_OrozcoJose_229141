126b99eb1e61679fee32838047d0bd3a8f6b1e82
Jose\Jose <jose.orozco229141@potros.itson.edu.mx>
2023-02-09 01:44:51 -0700

    Version Final de Calculadora, con aspectos mejorados

--- Calculadora/app/src/main/java/mx/edu/itson/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/mx/edu/itson/calculadora/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     var numeroActual: String="0"
-
+    var detonante:Boolean=false
     var operacionActual:Int=-1
 
    fun numerousActual():Unit{
@@ -22,10 +22,13 @@ class MainActivity : AppCompatActivity() {
        val ocho: Button =findViewById(R.id.btn8)
        val nueve: Button =findViewById(R.id.btn9)
        val cero: Button =findViewById(R.id.btn0)
+       val error: TextView =findViewById(R.id.txError)
 
        val numero: TextView =findViewById(R.id.txNumero)
 
        uno.setOnClickListener {
+          if(detonante==false){
+
            if(numeroActual.length==1){
                if(numeroActual=="0"){
                    numeroActual="1"
@@ -37,123 +40,246 @@ class MainActivity : AppCompatActivity() {
                numeroActual=numeroActual+"1"
            }
            numero.setText(numeroActual)
+          }else{
+              if(operacionActual==-1){
+                  val mensaje2="Aun no ha seleccionado una operacion"
+                  error.text = mensaje2
+              }else {
+                  detonante = false
+                  numeroActual = "1"
+                  numero.setText(numeroActual)
+              }
+          }
        }
        dos.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="2"
-               }
-               else{
+
+           if(detonante==false){
+
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="2"
+                   }
+                   else{
+                       numeroActual=numeroActual+"2"
+                   }
+               }else{
                    numeroActual=numeroActual+"2"
                }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"2"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else {
+                   detonante = false
+                   numeroActual = "2"
+                   numero.setText(numeroActual)
+               }
            }
-           numero.setText(numeroActual)
+
+
        }
        tres.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="3"
-               }
-               else{
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="3"
+                   }
+                   else{
+                       numeroActual=numeroActual+"3"
+                   }
+               }else{
                    numeroActual=numeroActual+"3"
                }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"3"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else{
+               detonante=false
+               numeroActual="3"
+               numero.setText(numeroActual)
+               }
+
            }
-           numero.setText(numeroActual)
+
+
        }
        cuatro.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="4"
-               }
-               else{
+
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="4"
+                   }
+                   else{
+                       numeroActual=numeroActual+"4"
+                   }
+               }else{
                    numeroActual=numeroActual+"4"
                }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"4"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else{
+               detonante=false
+               numeroActual="4"
+               numero.setText(numeroActual)
+               }
            }
-           numero.setText(numeroActual)
+
        }
        cinco.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="5"
-               }
-               else{
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="5"
+                   }
+                   else{
+                       numeroActual=numeroActual+"5"
+                   }
+               }else{
                    numeroActual=numeroActual+"5"
                }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"5"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else{
+               detonante=false
+               numeroActual="5"
+               numero.setText(numeroActual)
+               }
            }
-           numero.setText(numeroActual)
+
        }
        seis.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="6"
-               }
-               else{
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="6"
+                   }
+                   else{
+                       numeroActual=numeroActual+"6"
+                   }
+               }else{
                    numeroActual=numeroActual+"6"
                }
+               numero.setText(numeroActual)
+           }else{     if(operacionActual==-1){
+               val mensaje2="Aun no ha seleccionado una operacion"
+               error.text = mensaje2
            }else{
-               numeroActual=numeroActual+"6"
+               detonante=false
+               numeroActual="6"
+               numero.setText(numeroActual)
            }
-           numero.setText(numeroActual)
+           }
+
        }
        siete.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="7"
-               }
-               else{
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="7"
+                   }
+                   else{
+                       numeroActual=numeroActual+"7"
+                   }
+               }else{
                    numeroActual=numeroActual+"7"
                }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"7"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else{
+               detonante=false
+               numeroActual="7"
+               numero.setText(numeroActual)
+               }
            }
-           numero.setText(numeroActual)
+
        }
        ocho.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="8"
-               }
-               else{
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="8"
+                   }
+                   else{
+                       numeroActual=numeroActual+"8"
+                   }
+               }else{
                    numeroActual=numeroActual+"8"
                }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"8"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else{
+               detonante=false
+               numeroActual="8"
+               numero.setText(numeroActual)
+               }
            }
-           numero.setText(numeroActual)
+
        }
        nueve.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="9"
-               }
-               else{
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="9"
+                   }
+                   else{
+                       numeroActual=numeroActual+"9"
+                   }
+               }else{
                    numeroActual=numeroActual+"9"
                }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"9"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else{
+               detonante=false
+               numeroActual="9"
+               numero.setText(numeroActual)
+               }
            }
-           numero.setText(numeroActual)
+
        }
        cero.setOnClickListener {
-           if(numeroActual.length==1){
-               if(numeroActual=="0"){
-                   numeroActual="0"
+           if(detonante==false){
+               if(numeroActual.length==1){
+                   if(numeroActual=="0"){
+                       numeroActual="0"
+                   }
+                   else{
+                       numeroActual=numeroActual+"0"
+                   }
+               }else{
+                   numeroActual=numeroActual+"O"
                }
-               else{
-                   numeroActual=numeroActual+"0"
-               }
+               numero.setText(numeroActual)
            }else{
-               numeroActual=numeroActual+"O"
+               if(operacionActual==-1){
+                   val mensaje2="Aun no ha seleccionado una operacion"
+                   error.text = mensaje2
+               }else{
+               detonante=false
+               numeroActual="0"
+               numero.setText(numeroActual)
+               }
            }
-           numero.setText(numeroActual)
+
        }
    }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -182,6 +308,8 @@ class MainActivity : AppCompatActivity() {
                 numeroB=numero.text.toString().toDouble()
                 total.setText((numeroA+numeroB).toString())
                 numeroActual=(numeroA+numeroB).toString()
+                numero.setText("0")
+                detonante=true
                 operacionActual=-1
             }
 
@@ -190,6 +318,8 @@ class MainActivity : AppCompatActivity() {
                 numeroB=numero.text.toString().toDouble()
                 total.setText((numeroA-numeroB).toString())
                 numeroActual=(numeroA-numeroB).toString()
+                numero.setText("0")
+                detonante=true
                 operacionActual=-1
             }
 
@@ -198,33 +328,44 @@ class MainActivity : AppCompatActivity() {
                 numeroB=numero.text.toString().toDouble()
                 total.setText((numeroA*numeroB).toString())
                 numeroActual=(numeroA*numeroB).toString()
+                numero.setText("0")
+                detonante=true
                 operacionActual=-1
             }
-
               if(operacionActual==4){
                   numeroA=total.text.substring(0,total.text.length-1).toDouble()
                   numeroB=numero.text.toString().toDouble()
-                  if(numeroA==0.0){
+                  if(numeroB==0.0){
                       error.text="No se puede dividir entre 0, borre lo realizado"
                   }else{
+                      if(numeroA==0.0){
+                          total.setText(("0").toString())
+                          numeroActual=("0").toString()
+                          operacionActual=-1
+                          numero.setText("0")
+                      }else{
                       total.setText((numeroA/numeroB).toString())
                       numeroActual=(numeroA/numeroB).toString()
                       operacionActual=-1
+                      numero.setText("0")
+                      }
+                      detonante=true
                   }
             }
         }
-
         borrar.setOnClickListener {
             numero.setText("")
             total.setText("")
             error.setText("")
             numeroActual="0"
             operacionActual=-1;
+            detonante=false
         }
     }
 
     fun operacion():Unit{
         val total: TextView =findViewById(R.id.txTotal)
+        val error: TextView =findViewById(R.id.txError)
         val numero: TextView =findViewById(R.id.txNumero)
         val mas: Button =findViewById(R.id.btnMas)
         val menos: Button =findViewById(R.id.btnMenos)
@@ -232,15 +373,18 @@ class MainActivity : AppCompatActivity() {
         val multiplicar: Button =findViewById(R.id.btnMultiplicar)
         mas.setOnClickListener {
             if(operacionActual==-1) {
+
                 total.setText(numeroActual.plus("+") )
+                operacionActual = 1
+                numero.setText("0")
+               numeroActual="0"
+            }else{
+                total.setText(total.text.substring(0,total.text.length-1).toString().plus("+") )
                 operacionActual = 1
                 numero.setText("0")
                 numeroActual="0"
-            }else{
-                total.setText(numeroActual.plus("+") )
-                operacionActual = 1
-                numero.setText("0")
             }
+            error.setText("")
         }
         menos.setOnClickListener {
             if(operacionActual==-1) {
@@ -249,10 +393,14 @@ class MainActivity : AppCompatActivity() {
                 numero.setText("0")
                 numeroActual="0"
             }else{
-                total.setText(numeroActual.plus("-") )
+                total.setText(total.text.substring(0,total.text.length-1).toString().plus("-") )
                 operacionActual = 2
                 numero.setText("0")
+                numeroActual="0"
+
             }
+            error.setText("")
+
         }
 
         multiplicar.setOnClickListener {
@@ -262,10 +410,13 @@ class MainActivity : AppCompatActivity() {
                 numero.setText("0")
                 numeroActual="0"
             }else{
-                total.setText(numeroActual.plus("*") )
+                total.setText(total.text.substring(0,total.text.length-1).toString().plus("*") )
                 operacionActual=3
                 numero.setText("0")
+                numeroActual="0"
             }
+            error.setText("")
+
         }
 
         dividir.setOnClickListener {
@@ -275,10 +426,13 @@ class MainActivity : AppCompatActivity() {
                 numero.setText("0")
                 numeroActual="0"
             }else{
-                total.setText(numeroActual.plus("/") )
+                total.setText(total.text.substring(0,total.text.length-1).toString().plus("/") )
                 operacionActual = 4
+                numeroActual="0"
                 numero.setText("0")
             }
+            error.setText("")
+
         }
     }
 
